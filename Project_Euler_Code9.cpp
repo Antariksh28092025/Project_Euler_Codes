f12078b342141d6c479f11199d6015da4d33afcb
Antariksh28092025 <antarikshsa2003@gmail.com>
2026-09-09 17:44:36 +0530

    Add Project_Euler_code 9

--- Project_Euler_Code9.cpp
+++ Project_Euler_Code9.cpp
@@ -1,6 +1,28 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int a , b , c ;
-    for (int a = 0 , int a^2+b^2 = c^2 ;  ) 
+#include <iostream>
+
+int main() {
+    constexpr int target_sum = 1000;
+    int loop_count = 0;
+
+    for (int a = 1; a < target_sum / 3; ++a) {
+        for (int b = a + 1; b < (target_sum - a) / 2; ++b) {
+            ++loop_count;
+
+            int c = target_sum - a - b;
+
+            if (a * a + b * b == c * c) {
+                std::cout << "a = " << a
+                          << ", b = " << b
+                          << ", c = " << c << '\n';
+
+                std::cout << "a * b * c = " << a * b * c << '\n';
+                std::cout << "Loop count: " << loop_count << '\n';
+
+                return 0;
+            }
+        }
+    }
+
+    std::cout << "No triplet found.\n";
+    return 0;
 }
